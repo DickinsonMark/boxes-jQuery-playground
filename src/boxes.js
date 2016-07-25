9175a824ee74909ee1943c3736b92c56ebbb24f2
DickinsonMark <monoposon@comcast.net>
2016-07-25 13:51:31 -0600

    made it most of the way through the third exersize

--- src/boxes.js
+++ src/boxes.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
-  alert('ready for DOM manipulation.');
-}
+  'use strict';
+
+  // exercise 1
+  // alert('ready for DOM manipulation!');
+
+  // exercise 2
+  // 1
+  // $('#secretBox').css({backgroundColor: 'white'}).append('<h1>secret box!');
+  // 2
+  // $('#row1 > div').removeClass().addClass('box boxType3');
+  // 3
+  // $('#row4 div:last-child').css({display: 'none'});
+  // 4
+  // $('#row2 > div:first-child, #row2 > div:first-child + div').removeClass();
+  // 5
+  // $('.boxType1').css({backgroundColor: 'white'});
+  // 6
+  // $('#container :not(.row, #secretBox)').css({width: '2px'})
+
+  // exercise 3
+  // 1
+  $('#container').on('click', function(e) {
+    console.log('x: ' + e.pageX + 'y: ' + e.pageY);
+  })
+
+  // 2
+  $('.boxType1').append('<a href="http://www.galvanize.com">Go to galvanize</a>');
+  $('a').on('click', function(event) {
+
+    $(this).attr('href', ' ');
+    alert('You can never leave this page!');
+  })
+
+  // 3
+  $('.box').on('click', function(event) {
+    $(this).css({backgroundImage: 'cute-puppy.jpg'});
+  })
+});
